d133292f40e679cd5e4b20fdec7e3715f940d726
chandrashekar <139521045+chandu12121@users.noreply.github.com>
2025-03-03 15:45:33 +0530

    Update Doctor.js

--- models/Doctor.js
+++ models/Doctor.js
@@ -1,19 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const doctorSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     specialty: { type: String, required: true },
-//     experience: { type: Number, required: true },
-//     contact: { type: String, required: true },
-//     workingHours: { 
-//         start: { type: Number, required: true }, 
-//         end: { type: Number, required: true },   
-//     }
-// });
-
-// const Doctor = mongoose.model("Doctor", doctorSchema);
-// module.exports = Doctor;
-
 const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema({
@@ -22,8 +6,8 @@ const doctorSchema = new mongoose.Schema({
     experience: { type: Number, required: true },
     contact: { type: String, required: true },
     workingHours: {
-        start: { type: String, required: true }, // Change to String
-        end: { type: String, required: true }    // Change to String
+        start: { type: String, required: true }, 
+        end: { type: String, required: true }    
     }
 });
 
